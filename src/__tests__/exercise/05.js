050575be8dc735c916d7fa255e552f8343b94239
Juan Arias <jcamiloat@gmail.com>
2023-09-04 06:43:20 -0500

    feat: test05 extra03 - Using inline snapshot

--- src/__tests__/exercise/05.js
+++ src/__tests__/exercise/05.js
@@ -62,8 +62,8 @@ test(`logging in displays the user's username`, async () => {
 });
 
 test('An error is displayed when a field is missing', async () => {
-  render(<Login />);
-  const { username, password } = buildLoginForm();
+  const { asFragment } = render(<Login />);
+  const { username } = buildLoginForm();
 
   await userEvent.type(screen.getByLabelText(/username/i), username);
   // 🐨 uncomment this and you'll start making the request!
@@ -78,5 +78,52 @@ test('An error is displayed when a field is missing', async () => {
   // once the login is successful, then the loading spinner disappears and
   // we render the username.
   // 🐨 assert that the username is on the screen
-  expect(screen.getByText('password required')).toBeInTheDocument();
+  // expect(screen.getByText('password required')).toBeInTheDocument();
+  expect(asFragment()).toMatchInlineSnapshot(`
+    <DocumentFragment>
+      <form>
+        <div>
+          <label
+            for="username-field"
+          >
+            Username
+          </label>
+          <input
+            id="username-field"
+            name="username"
+            type="text"
+          />
+        </div>
+        <div>
+          <label
+            for="password-field"
+          >
+            Password
+          </label>
+          <input
+            id="password-field"
+            name="password"
+            type="password"
+          />
+        </div>
+        <div>
+          <button
+            type="submit"
+          >
+            Submit
+          </button>
+        </div>
+      </form>
+      <div
+        style="height: 200px;"
+      >
+        <div
+          role="alert"
+          style="color: red;"
+        >
+          password required
+        </div>
+      </div>
+    </DocumentFragment>
+  `);
 });
